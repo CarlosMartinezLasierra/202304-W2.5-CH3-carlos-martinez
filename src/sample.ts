18a962591680176ca71f597914d16b4195c07f38
Carlos Martinez <carlosmartinezlasierra@gmail.com>
2023-05-11 06:42:24 +0200

    Add sample.test.ts to test fizzbuzz function

--- src/sample.ts
+++ src/sample.ts
@@ -1,1 +1,18 @@
+function fizzbuzz(start: number, end: number): Array<string | number> {
+  const result: Array<string | number> = [];
+
+  for (let num = start; num <= end; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (num % 3 === 0) {
+      result.push("Fizz");
+    } else if (num % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(num);
+    }
+  }
+
+  return result;
+}
 
